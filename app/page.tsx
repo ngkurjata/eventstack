d2cf664ef5d6869b4f73ab6898483e427f7c3496
Nathan Kurjata <ngkurjata@gmail.com>
2026-02-14 19:01:14 -0800

    Fix select() null error

--- app/page.tsx
+++ app/page.tsx
@@ -645,7 +645,7 @@ export default function Page() {
       <div className="mx-auto max-w-3xl px-4 py-10 sm:py-14">
         <header className="mb-8">
           <h1 className="text-center text-3xl font-black tracking-tight text-slate-900 sm:text-4xl">
-            Find an epic trip!</h1>
+            Find an epic trip!
           <p className="mt-7 text-center text-sm text-slate-600 sm:text-base">
           We help users find and book epic trips built around live events featuring their favorite teams and artists.
           </p>
